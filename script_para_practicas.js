9c02a84580767a752098fa19e527f1f2603654c3
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-11-13 15:33:38 -0500

    Hcaiendo mini practicas antes de avnzar a control de flujo practicando domm

--- script_para_practicas.js
+++ script_para_practicas.js
@@ -1,11 +1,10 @@
-console.log("Hola mundo ");
-
-
 const selecionarh2 = document.querySelectorAll("h2");
-console.log(selecionarh2);
 
 for (let i = 0; i < selecionarh2.length; i++) {
     selecionarh2[i].textContent = "Hola a todos los h2"
-    
 }
+//obtener nombre y cambiar las clase
 
+const selecion_de_Clases = document.querySelectorAll(".card");
+//selecion_de_Clases.setAttribute("card_especificadas");
+//console.log(selecion_de_Clases)
