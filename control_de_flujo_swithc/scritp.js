a48979aa01c3cb1ec498a5534c2af417b91ba4e8
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-11-19 18:26:19 -0500

    repasando case break

---
+++ control_de_flujo_swithc/scritp.js
@@ -0,0 +1,16 @@
+ 
+ 
+ let key  = "banana"
+ switch (key) {
+    case "banana": 
+    console.log("es muy amarrilla");
+    break;
+    case "manzana": 
+    console.log("los primero humanos pecaron");
+    break;
+    case "pera": 
+    console.log("muy deleciosa pero es verde");
+    break;
+ }
+
+ 
