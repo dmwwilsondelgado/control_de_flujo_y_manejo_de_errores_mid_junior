f0f30ca0698ff9a73a2cf29897c5d4b4644cb87c
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-11-21 15:33:19 -0500

    interfaz de profesor para nota

---
+++ html/script_para_practicas.js
@@ -0,0 +1,9 @@
+console.clear()/// limpiaos la consola para hacer las practicas necesarias para aplicar flijo y control 
+// un profesor rompio el sisstema lamentablemenet ay que crear la solucion al problema desarrolando una web similiar a su funcionamiento
+const selecionarh2 = document.querySelector("h2");
+selecionarh2.textContent = "Introduce la nota del estudiante";
+console.log(selecionarh2);
+
+
+
+
