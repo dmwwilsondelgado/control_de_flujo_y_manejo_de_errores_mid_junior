a3be7daed85452b7c53b7a9e29c26fed9411eeab
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-11-22 15:59:49 -0500

    ya repasamos

--- html/script_para_practicas.js
+++ html/script_para_practicas.js
@@ -5,32 +5,43 @@ selecionarh2.textContent = "Introduce la nota del estudiante";
 //console.log(selecionarh2);
 const sendButoon = document.getElementById(`snd-nota`);
 //console.log(seendButoon);
-
 sendButoon.addEventListener("click",()=>{
     // creamos una funcion dentro de una funcion anonima
     let resultado,mensaje;
     try {
-        resultado = parseInt(document.getElementById("nota").value);
-        if (isNaN(resultado)) {
-            throw "Gracioso"
+        preRes = parseInt(document.getElementById("nota").value);
+        if (isNaN(preRes)) {
+            throw "Gracioso";
             
         }
-        verificarAprovacion();
-        mensaje = " hola";
+        //dependiendo del resultado lo enviamso a otra funcion donde esta evalua
+        mensaje = definirMensaje(preRes);
+        resultado =  verificarAprobacion(preRes);
     } catch (error) {
         resultado = "sos gracioso ?";
         mensaje =  alert("estas descupierto que intentaste hackear el sitio ");
     }
-    abrirmodal(resultado,mensaje)    
+    abrirmodal(resultado,mensaje);
 })
-
-
 
 const abrirmodal = (res,mens) => {
     console.log(res,);
     console.log(mens);
 }
 
-const verificarAprovacion  = () => {
-    
+const definirMensaje = (pr) => {
+    let resultado;
+    switch (pr) {
+        case 1: resultado = "No podes ser Tan HDP"
+        break;
+        case 2: resultado = "No podes ser Tan Desaplicado en Programacion"
+        break;
+        case 3: resultado = "No podes ser mas Diciplinado en Base de datos"
+        break;
+        case 4: resultado = "No podes ser tan afortunado en Javascript"
+        break;
+        case 5: resultado = "No podes ser Demasiado Para Java"
+        break;
+        default: resultado = null;
+    }
 }
