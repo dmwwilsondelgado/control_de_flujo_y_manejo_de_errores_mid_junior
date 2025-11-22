3622080bd0dfa594b8568f90eaa9fc050d010354
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-11-22 14:58:23 -0500

    prueba al cambiar elementos desde elemente prueba y erorr

--- html/script_para_practicas.js
+++ html/script_para_practicas.js
@@ -2,10 +2,30 @@ console.clear()/// limpiaos la consola para hacer las practicas necesarias para 
 // un profesor rompio el sisstema lamentablemenet ay que crear la solucion al problema desarrolando una web similiar a su funcionamiento
 const selecionarh2 = document.querySelector("h2");
 selecionarh2.textContent = "Introduce la nota del estudiante";
-console.log(selecionarh2);
-const seendButoon = document.getElementById(`snd-nota`);
-console.log(seendButoon);
+//console.log(selecionarh2);
+const sendButoon = document.getElementById(`snd-nota`);
+//console.log(seendButoon);
 
-seendButoon.addEventListener("click",()=>{
-    // creamos una funcion dentro de una funcion anonima    
+sendButoon.addEventListener("click",()=>{
+    // creamos una funcion dentro de una funcion anonima
+    let resultado,mensaje;
+    try {
+        resultado = parseInt(document.getElementById("nota").value);
+        mensaje = "hola"
+        if (resultado.isNaN()) {
+            throw "Gracioso"
+            
+        }
+    } catch (error) {
+        resultado = "sos gracioso ?";
+        mensaje =  alert("E descupierto que intentaste hackear el sitio ");
+    }
+    abrirmodal(resultado,mensaje)    
 })
+
+
+
+const abrirmodal = (res,mens) => {
+    console.log(res,);
+    console.log(mens);
+}
